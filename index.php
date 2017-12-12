fd1c9ad4102ae7eef84b1b12d9d0a2f7545b7af3
AndreVictor60 <andre.victor60@free.fr>
2017-12-12 21:34:08 +0100

    fini

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Partie 5 Exercice 9</title>
+  </head>
+  <body>
+    <?php
+      $hautDeFrance = array(60=>'Oise',80=>'Somme',02=>'Aisne',62=>'Pas-de-calais',59=>'Nord');
+      print_r($hautDeFrance);
+     ?>
+  </body>
+</html>
